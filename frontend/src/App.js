595f97a5b3b4d04cfb028a63aafedb8d2f2ed75a
Bella <Bella.Sargsyan@yahoo.com>
2022-12-02 16:38:30 +0400

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
+//Screens
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
+
+// Components
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <Router>
+      < Navbar />
+    {/* SideDrawer */}
+    {/* Backdrop */}
+    
+      <main >
+        <Routes>
+          <Route  path="/"element={<HomeScreen />} />
+          <Route  path="/product/:id" element={<ProductScreen />} />
+          <Route  path="/cart" element={<CartScreen />} />
+        </Routes>
+      </main>
+    </Router>
+ 
+
+
+  );
+
+  
+}
+
+export default App;
