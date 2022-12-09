d57fd63e7787526b2b4660b93df40fdb374bf7d9
Bella <Bella.Sargsyan@yahoo.com>
2022-12-09 18:14:55 +0400

    add redux

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,20 +1,23 @@
 import './HomeScreen.css';
 
+// Components
 import Product from '../components/Product';
 
-
- const HomeScreen = () => {
+const HomeScreen = () => {
     return (
      <div className='homescreen'>
       <h2 className='homescreen_title'>Latest Products</h2>
 
       <div className='homescreen_products'>
         <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
       </div>
-
     </div>
-    );
-    
+  );
 };
 
 export default HomeScreen;
